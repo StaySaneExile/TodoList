cf2ac6306a35b263653069378144a25a6c469173
StaySaneExile <vasiliy2490@gmail.com>
2020-07-10 16:25:09 +0500

    Updates

---
+++ precache-manifest.9e49fb400ef6126bd2137c05aba36c6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a46011624f4b0412c81c288a2b1d559",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "12a98bc219dfb982356c",
+    "url": "/TodoList/static/css/main.453f9617.chunk.css"
+  },
+  {
+    "revision": "8e2c8ef7bbbd854a1040",
+    "url": "/TodoList/static/js/2.59d346c4.chunk.js"
+  },
+  {
+    "revision": "12a98bc219dfb982356c",
+    "url": "/TodoList/static/js/main.f42dbf42.chunk.js"
+  },
+  {
+    "revision": "23ef1ee30c4a1e2cab36",
+    "url": "/TodoList/static/js/runtime~main.90b1048d.js"
+  },
+  {
+    "revision": "052865c3f522f47af269d5736641d5ee",
+    "url": "/TodoList/static/media/backgr.052865c3.jpg"
+  },
+  {
+    "revision": "2ea58cae5c4b2883a49f2b671ff98975",
+    "url": "/TodoList/static/media/delete.2ea58cae.png"
+  }
+]);
